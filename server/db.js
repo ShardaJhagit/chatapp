ffcf6e00ea708e786b925d876bf7465f6fcc477c
Sharda Jha <jhas1203@gmail.com>
2023-08-03 18:20:53 +0530

    commit now
    commit now

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const nodemailer=require("nodemailer")
 const port = 8080;
 
 const db=mysql.createPool({
-    host:'localhost',
+    host:'',
     user:'root',
     password:'Sjsql@1234',
     database:'sj'
