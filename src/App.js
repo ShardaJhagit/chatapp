729635607b1388eba88424b4a85128b3ea1a49df
Sharda Jha <jhas1203@gmail.com>
2023-08-03 11:11:36 +0530

    again
    again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "react-router-dom"
 import './App.css';
 import Home from './pages/Home';
-//import Services from './pages/Services';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Navi from './pages/Navi';
 import Login from './pages/Login';
 import Private from './pages/Private';
 import Register from './pages/Register';
-//import Enroll from './pages/Enroll';
 import Chat from './pages/Chat';
-import Log from './pages/Log';
-import ChatPrivacy from './pages/ChatPrivacy';
+
 
 
 
@@ -25,15 +22,12 @@ function App() {
       
       <Routes>
         <Route element={<Private/>}>
-        
-          
-          
         <Route path="/" element={ <Home/> } />
         <Route path="/about" element={ <About/> } />
         <Route path="/contact" element={ <Contact/> } />
         
         <Route path="/chat" element={ <Chat/> } />
-        <Route path="/username" element={ <Log/> } />
+        
         </Route>
         
         
